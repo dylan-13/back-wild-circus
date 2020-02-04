320bf509e3006aa8ce239b7f67812dc4f8905148
Dylan <dylan.berthier13@gmail.com>
2020-02-04 17:02:44 +0100

    final for deploy

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const models = require('./models')
+var port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
-app.use(bodyParser.urlencoded({extended:true}))// pour les params
+app.use(bodyParser.urlencoded({ extended: true }))// pour les params
 app.use(bodyParser.json())
 
 require('./routes/student.js')(app)
 
 models
     .sequelize
-    .sync({force: false})
-    .then(() => app.listen(5000, () => console.log('server is listenning')))
+    .sync({ force: false })
+    .then(() => app.listen(port, () => console.log('server is listenning')))
